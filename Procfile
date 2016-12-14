c9d74e7273d853a98edbb6a84b38d632bdd80a10
ArunK <arunkarnaa@gmail.com>
2016-12-14 14:51:55 +0530

    Heroku deploy
    Heroku deployed Telegram bot

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python telegramTest.py --log-file -
--- telegramTest.py
+++ telegramTest.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*- 
+
 import telebot
 import xml.etree.ElementTree as et
 import random
 from telebot import types
 import urllib
+import time
+
+
+
 
 source_xml_file = "kural.xml"
 user_id_txt_file = "userid.txt"
@@ -10,22 +16,19 @@ tree = et.parse(source_xml_file)
 element_top = tree.getroot()
 
 
-
-bot = telebot.TeleBot("<APITOKEN>")
+api_token = "<api_token>"
+bot = telebot.TeleBot(api_token)
 bot.set_webhook()
 
-@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
-def on_user_joins(m):
-   cid = m.chat.id
-   if(m.content.content_type='new_chat_participant'):
-      if m.new_chat_
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
    with open(user_id_txt_file, 'a') as f:
                 print(str(message.from_user.id), file=f)
+
    print(str(message.from_user.id))
-   bot.send_message(message.chat.id,"அகர முதல எழுத்தெல்லாம் ஆதி \n பகவன் முதற்றே உலகு.")
+   bot.send_message(message.chat.id,"அகர முதல எழுத்தெல்லாம் ஆதி  பகவன் முதற்றே உலகு.")
    
    
 @bot.message_handler(commands=['random'])
@@ -35,12 +38,12 @@ def random_quote(message):
    the_message = "குறள் எண்: " +str(temp_number)+ "\n\n"+element_top[temp_number][1].text+"\n"+element_top[temp_number][2].text+ "\n\n" +element_top[temp_number][3].text+"\n\n"+"- திருவள்ளுவர்" 
    bot.send_message(message.chat.id,the_message)
 
-
-
 try: 
     bot.polling(none_stop=True)
 except urllib.error.HTTPError:
     time.sleep(10)
+
+
 
 
 while True:
